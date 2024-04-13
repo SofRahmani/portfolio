e2f8946b5cfd1578b229985a7e31e49ade2714a0
SofRahmani <sofiane.rahmani12@gmail.com>
2024-04-14 01:19:15 +0200

    feat: :sparkles: create Separator component

---
+++ src/app/[lang]/components/sections/Separator.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Separator() {
+  return (
+    <div className=' h-28 w-1 rounded-xl bg-violet-700 '>
+      
+    </div>
+  );
+}
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,10 +1,12 @@
 import { Locale } from "../../../i18n.config";
+import Separator from './components/sections/Separator'
 import HeroSection from "./components/sections/hero_section/HeroSection";
 
 export default function Home({ params }: { params: { lang: Locale } }) {
   return (
     <main className=" mx-auto mt-6 flex max-w-7xl flex-col items-center justify-center gap-8 ">
       <HeroSection lang={params.lang} />
+      <Separator />
     </main>
   );
 }
