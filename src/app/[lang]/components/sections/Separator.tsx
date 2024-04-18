b579203cdbf0be12aa53395c7a9f478be7fc846c
SofRahmani <sofiane.rahmani12@gmail.com>
2024-04-18 22:43:53 +0200

    feat: :dizzy: create separator + animation

--- src/app/[lang]/components/sections/Separator.tsx
+++ src/app/[lang]/components/sections/Separator.tsx
@@ -1,9 +1,43 @@
+'use client'
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
 
+const fadeInAnimationVariants = {
+  hidden: {
+    opacity: 0,
+    height: 0
+  },
+  visible: () => ({
+    opacity: 1,
+    height: 100,
+    transition: {
+      duration: 0.6
+    }
+  })
+};
 
 export default function Separator() {
+
+  const control = useAnimation();
+  const [ ref, inView ] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      control.start("visible");
+    } else {
+      control.start("hidden");
+    }
+
+  }, [control, inView])
+
   return (
-    <div className=' h-28 w-1 rounded-xl bg-violet-700 '>
-      
-    </div>
+    <motion.div
+      className=" h-28 w-1 rounded-full bg-violet-700 "
+      ref={ref}
+      variants={fadeInAnimationVariants}
+      initial="hidden"
+      animate={control}
+    />
   );
 }
