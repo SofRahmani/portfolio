82a580c954ae3b54085cdc4915f8a83970bc1b38
SofRahmani <sofiane.rahmani12@gmail.com>
2024-09-07 16:06:50 -0400

    refactor: :lipstick: Testimonials carousel

--- src/app/[lang]/components/sections/testimoniels_section/TestimonialsInfiniteScroll.tsx
+++ src/app/[lang]/components/sections/testimoniels_section/TestimonialsInfiniteScroll.tsx
@@ -1,16 +1,34 @@
 "use client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { LoaderCircle } from "lucide-react";
 import useSWR from "swr";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../ui/card";
 
-import { InfiniteMovingCards } from "../../ui/infinite-moving-cards";
+interface TestimonialsInfiniteScrollProps {
+  noTestimonials: string;
+}
 
-export function TestimonialsInfiniteScroll() {
-
+export function TestimonialsInfiniteScroll({ noTestimonials }: TestimonialsInfiniteScrollProps) {
   const fetcher = (url: string) => fetch(url).then(res => res.json());
-  const { data, error, isLoading } = useSWR("https://avis.sofiane-rahmani.com/api/testimonials", fetcher);
+  const { data, error, isLoading } = useSWR(
+    "https://avis.sofiane-rahmani.com/api/testimonials",
+    fetcher
+  );
   console.log(data);
 
   if (isLoading) {
-    return <div>Loading...</div>; // Show loading state
+    return (
+      <div className="flex size-full items-center justify-center">
+        <LoaderCircle className="size-10 animate-spin" />
+      </div>
+    ); 
   }
 
   if (error) {
@@ -18,14 +36,50 @@ export function TestimonialsInfiniteScroll() {
   }
 
   if (!data || data.length === 0) {
-    return <div>No testimonials found.</div>; // Handle case when data is empty or undefined
+    return (
+      <div className="flex size-full items-center justify-center italic">
+        {noTestimonials}
+      </div>
+    ); // Handle case when data is empty or undefined
   }
 
   return (
-    <div className="relative flex h-full flex-col items-center justify-center overflow-hidden rounded-md antialiased ">
-      <InfiniteMovingCards items={data} direction="right" speed="slow" className='' />
+    <div className="relative flex h-full items-center justify-center overflow-hidden rounded-md antialiased">
+      <Carousel
+        orientation="horizontal"
+        opts={{
+          align: "start",
+          loop: true
+        }}
+        plugins={[
+          Autoplay({
+            delay: 7000
+          })
+        ]}
+        className="w-3/4 lg:w-3/4"
+      >
+        <CarouselContent>
+          {data.map((item: any, index: number) => (
+            <CarouselItem key={index} className="lg:basis-1/2">
+              <Card className=" size-full">
+                <CardHeader>
+                  <CardTitle className="bg-gradient-to-b from-[#FF1CF7] to-[#b249f8] bg-clip-text text-transparent">
+                    {item.name}
+                  </CardTitle>
+                  <CardDescription>{item.title}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <p className=" mt-2 leading-relaxed ">{item.message}</p>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <div className="hidden md:block">
+          <CarouselPrevious />
+          <CarouselNext />
+        </div>
+      </Carousel>
     </div>
   );
 }
-
-
--- src/app/[lang]/components/sections/testimoniels_section/TestimonialsSection.tsx
+++ src/app/[lang]/components/sections/testimoniels_section/TestimonialsSection.tsx
@@ -1,9 +1,8 @@
 import { getDictionary } from "@/lib/dictionary";
 import { title } from "@/style/site";
+import { Reveal } from "@/utils/Reveal";
 import { Locale } from "../../../../../../i18n.config";
-import { Reveal } from '@/utils/Reveal';
 import { TestimonialsInfiniteScroll } from "./TestimonialsInfiniteScroll";
-
 
 export default async function TestimonialsSection({ lang }: { lang: Locale }) {
   const { pages } = await getDictionary(lang);
@@ -13,10 +12,12 @@ export default async function TestimonialsSection({ lang }: { lang: Locale }) {
       id="testimonials"
       className=" relative z-40 flex size-full scroll-mt-0.5 flex-col items-center justify-center gap-6 p-8 font-sans md:scroll-mt-96 "
     >
-      <Reveal width='w-fit'>
-        <h2 className={` ${title({ size: "md", color: "violet" })} `}>{pages.testimonials.title}</h2>
+      <Reveal width="w-fit">
+        <h2 className={` ${title({ size: "md", color: "violet" })} `}>
+          {pages.testimonials.title}
+        </h2>
       </Reveal>
-      <TestimonialsInfiniteScroll />
+      <TestimonialsInfiniteScroll noTestimonials={pages.testimonials.noTestimonials} />
     </section>
   );
 }
